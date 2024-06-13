0f87ef812a5cbd810e5eacea4ae6bd32ccf4df9d
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2024-06-13 15:19:32 +0530

    Creating Ui and Cleark add for authentication

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+const RootLayout = ({children} :{children :ReactNode}) => {
+  return (
+    <div>
+        
+        {children}
+            
+    </div>
+  )
+}
+
+export default RootLayout
