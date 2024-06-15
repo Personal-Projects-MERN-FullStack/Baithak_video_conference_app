4f0180589349080d8973beccc3b1b20ce24bce0a
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2024-06-15 22:28:29 +0530

    Meeting Sheduling and Maintain Histroy

--- src/components/MeetingModal.tsx
+++ src/components/MeetingModal.tsx
@@ -47,7 +47,7 @@ const MeetingModal = ({
               </div>
             )}
             <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>{title}</h1>
-
+              {children}
             <Button
               className={"bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0"}
               onClick={handleClick}
